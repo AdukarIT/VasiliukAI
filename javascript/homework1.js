53b0b72263f7f3d5bdd280522745e6d969399b6e
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2019-04-27 09:03:38 +0300

    Create homework1.js

---
+++ javascript/homework1.js
@@ -0,0 +1,65 @@
+/Задача 1. (Квартиры.Подъезды)
+
+// let flatNumber = +prompt ('Введите номер квартиры');
+// if (flatNumber <= 20) {
+// 	console.log('Первый подъезд');
+// } else if (flatNumber > 20 && flatNumber <= 40) {
+// 	console.log ('Второй подъезд');
+// } else if (flatNumber > 40 && flatNumber <= 60) {
+// 	console.log ('Третий подъезд');
+// } else if (flatNumber > 60 && flatNumber <= 80) {
+// 	console.log ('Четвертый подъезд');
+// } else {
+// 	console.log ('Пятый подъезд');
+// } 
+
+//Задача 2. (марки автомобилей)
+
+// let carModel = prompt ('Write Car Brand');
+// switch (carModel) {
+// 	case ('tesla'): 
+// 	case ('TESLA'): 
+// 	case ('Tesla'): 
+// 	console.log(carModel + ' Made in USA'); break;
+// 	case ('Mercedes-Benz'):
+// 	case ('mercedes-benz'):
+// 	case ('Mercedes-benz'):
+// 	case ('Mercedes Benz'):
+// 	case ('mercedes benz'):
+// 	case ('Mercedes benz'):
+// 	case ('MB'):
+// 	case ('mb'):
+// 	case ('BMW'):
+// 	case ('bmw'):
+//	case ('audi'):
+//	case ('AUDI'):
+//	case ('Audi'):
+// 	console.log(carModel + ' Made in Germany'); break;
+// 	case ('Peugeot'):
+// 	case ('peugeot'):
+// 	console.log (carModel + ' Made in France'); break;
+// 	case ('Rolls-Royce'):
+// 	case ('rolls-royce'):
+// 	case ('Rolls Royce'):
+// 	case ('rolls royce'):
+// 	console.log(carModel + ' Made in England'); break;
+// 	default: console.log(carModel + ' Unknown place of origin');
+// }
+
+// Задача 3. Високосный год.
+// Год високосный, если он не кратен 4, но кратен 100. Или кратен 100, но при этом не кратен 400.
+
+// let year = +prompt ('Какой год и узнай будет ли он високосный?');
+// if ((!(year % 4) && year % 100) || !(year % 400)) {
+// 	console.log(year + ' Високосный год');
+// } else {
+// 	console.log(year + ' не високосный год');
+// }
+
+
+//Задача 4. Таблица умножения.
+
+// let x = +prompt('Введите любое число от 1 до 20:');
+// for (let y = 1; y <= 10; y++) {
+// 	console.log(x + '*' + y + '=' + (x * y));
+// }
