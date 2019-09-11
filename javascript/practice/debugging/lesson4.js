83ee6a20d27eca1da7e79c4fd184decbcfc04b53
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2019-09-11 19:16:47 +0300

    Update lesson4.js

--- javascript/practice/debugging/lesson4.js
+++ javascript/practice/debugging/lesson4.js
@@ -33,3 +33,81 @@ function children(pupil, desk) {
 	}
 }
 console.log(children(10, 0));
+
+// Создайте функцию, которая получает в качестве аргумента оценку по 10-балльной шкале. 
+// Для ошибочных аргументов (0, «B») функция возвращает текстовую ошибку, для верных – их текстовое описание: 
+
+let mark = +prompt('Enter your mark and get its equivalent');
+	switch (mark) {
+		default: 
+			alert('Error, does not exist');
+			break;
+		case(0):
+			alert('Error');
+			break;
+		case(1):
+		case(2):
+			alert('Unsatisfactory');
+			break;			
+		case(3):
+		case(4):
+			alert('Satisfactory');
+			break;
+		case(5):
+			alert('Almost good');
+			break;
+		case(6):
+			alert('Good');
+			break;
+		case(7):
+			alert('Very good');
+			break;
+		case(8):
+			alert('Almost excellent');
+			break;
+		case(9):
+			alert('Excellent');
+			break;			
+		case(10):
+			alert('Brilliant');
+			break;
+		}
+console.log(mark);
+
+//Используя цикл do..while, создайте функцию, которая принимает числа через prompt(), пока не будет введено число 0.
+
+function numbers() {
+	let a;
+	do {
+		a = +prompt ('Enter random number');
+	} while (a != 0);
+}
+numbers();
+
+//Перепишите функцию из задачи 5 так, чтобы она принимала числа, пока их сумма остаётся меньше 100
+//выводила эту сумму в консоль, а возвращала количество введённых чисел.
+
+function sum() {
+	let a; sum = 0; let i = 0;
+	do {
+		a = +prompt ('Enter number for sum');
+		sum += a;
+		console.log(sum);
+		i++;
+	} while (sum < 100);
+	return i;
+}
+sum();
+
+//Создайте функцию, которая принимает число в качестве аргумента и проверяет, является ли это число простым.
+
+function simpleNum(a) {
+	if (a == 2) {
+		return 'Simple number';
+	} else if (a % 2 == 0) {
+		return 'Isnt simple number'
+	} else {
+		return 'Simple number'
+	}
+}
+console.log(simpleNum(33));
