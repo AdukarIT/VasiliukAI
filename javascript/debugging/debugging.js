9baa4f76bd970813770999aa1cfd531c854d5a0b
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2020-01-22 13:47:26 +0200

    Update debugging.js

--- javascript/debugging/debugging.js
+++ javascript/debugging/debugging.js
@@ -1,252 +1,274 @@
+//1
 // Функция, которая принимает три числа: два первых катеты, третье гипотенуза.
+
 function pifagor (a, b, c) {
-	if ((a*a) + (b*b) == (c*c)) {
-		return true;
-	} else if ((a <= 0) || (b <= 0) || (c <= 0)) {
+	if ((isNaN(a)) || (isNaN(b)) || (isNaN(c))) {
 		return false;
+	} else if  ((a <= 0) || (b <= 0) || (c <= 0)) {
+		return false;
+	} else if  ((a*a) + (b*b) == (c*c)) {
+		return true;
 	} else {
 		return false;
 	}
 }
-console.log(pifagor(3, 4, 5));
+console.log(pifagor('avb', 4, 5));
 
+//2
 // Создайте функцию repeat(str, n), которая возвращает строку, состоящую из n повторений.
 
-function repeat(str = 'слово', n = 2) {
+// function wordRepeat (str, n) { 
+// 	let result = '';
+// 	for (let i = 1; i <= n; i++) {
+// 		result += str;
+// 	}
+// 	return result;
+// }
+// console.log(wordRepeat('word', 5));
+
+function wordRepeat (str = '', n = 2) { 
 	let result = '';
 	for (let i = 1; i <= n; i++) {
 		result += str;
 	}
 	return result;
 }
-console.log(repeat());
+console.log(wordRepeat());
 
+//3
 // Создайте функцию, которая принимает два аргумента - количетсво учеников в классе и количество парт в кабинетею
 // Функция возвращает строку вида "не хватает двух парт" / "1 лишняя парта".
 
-function children(pupil, desk) {
-	let n = pupil - (desk * 2);
-	if (n >= 1 ) {
-		return ('не хватает' + n +  'парт');
-	} else if (n <= 1) {
-		return ('парт достаточно');
+function howMuch(pupils, desk) {
+	let n = pupils - (desk * 2);
+	if (n % 2 != 0) {
+		n += 1;
+	}
+	if (n / 2 > 0) {
+		return (n / 2) + ' desks are missing';
+	} else if (n / 2 < 0) {
+		return ((n / 2) * -1) + ' extra desks';
+	} else {
+		return ('enough desks');
 	}
 }
-console.log(children(10, 0));
+console.log(howMuch(2, 4));
 
+//4
 // Создайте функцию, которая получает в качестве аргумента оценку по 10-балльной шкале. 
 // Для ошибочных аргументов (0, «B») функция возвращает текстовую ошибку, для верных – их текстовое описание: 
 
-let mark = +prompt('Enter your mark and get its equivalent');
-	switch (mark) {
-		default: 
-			alert('Error, does not exist');
-			break;
-		case(0):
-			alert('Error');
-			break;
-		case(1):
-		case(2):
-			alert('Unsatisfactory');
-			break;			
-		case(3):
-		case(4):
-			alert('Satisfactory');
-			break;
-		case(5):
-			alert('Almost good');
-			break;
-		case(6):
-			alert('Good');
-			break;
-		case(7):
-			alert('Very good');
-			break;
-		case(8):
-			alert('Almost excellent');
-			break;
-		case(9):
-			alert('Excellent');
-			break;			
-		case(10):
-			alert('Brilliant');
-			break;
-		}
-console.log(mark);
+function rating(mark) {
+	switch(mark) {
+		case 0: 
+			return 'Error';
+		case 1:
+			return 'Unsatisfactory';
+		case 2:
+			return 'Unsatisfactory';
+		case 3:
+			return 'Satisfactory';
+		case 4:
+			return 'Satisfactory';
+		case 5:
+			return 'Almost good';
+		case 6:
+			return 'Good';
+		case 7:
+			return 'Very good';
+		case 8:
+			return 'Almost excellent';
+		case 9:
+			return 'Excellent';
+		case 10:
+			return 'Brilliant';
+		default:
+			return 'Its not a mark';
+	}
+}
 
+let mark = +prompt('Enter your mark');
+console.log(rating(mark));
+
+//5
 //Используя цикл do..while, создайте функцию, которая принимает числа через prompt(), пока не будет введено число 0.
 
-function numbers() {
-	let a;
+function untilZero() {
+	let n;
 	do {
-		a = +prompt ('Enter random number');
-	} while (a != 0);
+		n = +prompt('Enter random number');
+	} while (n != 0);
 }
-numbers();
-
+untilZero();
 //Перепишите функцию из задачи 5 так, чтобы она принимала числа, пока их сумма остаётся меньше 100
 //выводила эту сумму в консоль, а возвращала количество введённых чисел.
 
-function sum() {
-	let a; sum = 0; let i = 0;
-	do {
-		a = +prompt ('Enter number for sum');
-		sum += a;
-		console.log(sum);
-		i++;
-	} while (sum < 100);
-	return i;
-}
-sum();
+// function hundred() {
+// 	let number; 
+//  	hundred = 0; 
+//  	let i = 0; 
+// 		do {
+// 			number = +prompt('Enter random number less than a hundred');
+// 			hundred += number;
+// 			i++
+// 		} while (hundred < 100);
+// 			console.log(i);
+// 	}
+// hundred();
 
 //Создайте функцию, которая принимает число в качестве аргумента и проверяет, является ли это число простым.
 
-function simpleNum(a) {
-	if (a == 2) {
-		return 'Simple number';
-	} else if (a % 2 == 0) {
-		return 'Isnt simple number'
-	} else {
-		return 'Simple number'
-	}
-}
-console.log(simpleNum(33));
+// function primeNumber(a) {
+// 	if (isNaN(a)) {
+// 		return 'It is not a number';
+// 	} else if (a == 2) {
+// 		return 'It is a prime number';
+// 	} else if (a % 2 == 0) {
+// 		return 'It is not a prime number';
+// 	} else {
+// 		return 'It is a prime number';
+// 	}
+// }
+// console.log(primeNumber('awcw'));
 
 //Создайте функцию, которая принимает в качестве аргумента целое число, соответствующее порядковому номеру месяца.
 //Если месяц с таким номером существует, функция возвращает имя сезона (лето, осень, зима, весна),
 //к которому относится месяц. Если нет –сообщение об ошибке.
 
-let monthNumber = +prompt('Enter number of month u wanna know');
-switch (monthNumber) {
-	default: 
-		alert('Error, does not exist');
-		break;
-	case (12):
-	case (1):
-	case (2):
-		alert('It is Winter');
-		break;
-	case (3):
-	case (4):
-	case (5):
-		alert('It is Spring');
-		break;
-	case (6):
-	case (7):
-	case (8):
-		alert('It is Summer');
-		break;
-	case (9):
-	case (10):
-	case (11):
-		alert('It is Autumn');
-		break;
-}
+// function seasons(month) {
+// 	switch(month) {
+// 		case 12:
+// 		case 1:
+// 		case 2:
+// 			return 'It is a winter';
+// 		case 3:
+// 		case 4:
+// 		case 5:
+// 			return 'It is a sping';
+// 		case 6:
+// 		case 7:
+// 		case 8:
+// 			return 'It is a summer';
+// 		case 9:
+// 		case 10:
+// 		case 11:
+// 			return 'It is a autumn';
+// 		default: 
+// 			return 'Error! Enter number less than 12';
+// 	}
+// }
+// console.log(seasons(15));
 
 //Создайте функцию, которая выводит в консоль числа от 10 до 99, 
 //заканчивающиеся на 7 или делящиеся на 7 (в убывающем порядке).
-
-function seven() {
-	for (let x = 99; x >= 10; x--) {
-		if (x % 7 == 0 || x % 10 == 7) {
-		console.log(x);
-		}
-	}
-}
-console.log(seven());
+// 
+// function seven()  {
+// 	for (let a = 99; a >= 10; a--) {
+// 		if (a % 7 == 0 || a % 10 == 7) {
+// 		console.log(a)
+// 		}
+// 	}
+// }
+// seven();
 
 //Создайте функцию, которая для чисел от 2 до 20 выводит в консоль все их делители.
-function del() {
-	for (var i = 2; i <= 20; i++) {
-		for (var j = 2; j <= 9; j++) {
-			if (i % j == 0) console.log(j + " делитель числа " + i);
-		}
-	}
-}
-console.log(del());
+
+// function divisor() {
+// 	for (let x = 2; x <=20; x++) {
+// 		for (let y = 2; y <= 9; y++) {
+// 			if (x % y == 0) console.log(y + " делитель числа " + x);
+// 		}
+// 	}
+// }
+// divisor();
+
 //сделать задание 10 в общем виде.
-function noName(x, y) {
-	for (var i = x; i <= y; i++) {
-			for (var j = x; j <= i; j++) {
-				if (i % j == 0) {
-				console.log(j + " делитель числа " + i);
-				}
-			}  
-	}
-}
-noName(2, 21);
+
+// function noName(x, y) {
+// 	for (let i = x; i <= y; i++) {
+// 		for (let j = x; j <= i; j++) {
+// 			if (i % j == 0) {
+// 				console.log(j + " делитель числа " + i);
+// 			}
+// 		}  
+// 	}
+// }
+// noName(1, 10);
 
 //Создайте функцию с параметрами size (число) и unit (строка). 
 //В unit передаются единицы измерения информации («Кб», «Мб», «Гб»), 
 //в size – количество таких единиц. Функция возвращает количество байт в size.
 
-function howMuchIs(size, unit) {
-	switch (unit) {
-		case "Кб":
-		case "кб":
-		case "кБ":
-		case "КБ":
-			return "В " + size + " Кб" + " содержится " + size * 1024 + " байт";
-			break;
-		case "Мб":
-		case "мб":
-		case "мБ":
-		case "МБ":
-			return "В " + size + " Мб" + " содержится " + size * 2 ** 20 + " байт";
-			break;
-		case "Гб":
-		case "гб":
-		case "гБ":
-		case "ГБ":
-			return "В " + size + " Гб" + " содержится " + size * 2 ** 30 + " байт";
-			break;
-		default: 
-			alert ("Корявые данные");
-			break;
-	}
-}
-console.log(howMuchIs(500, "Гб"));
+// function howMuchIs(size, unit) {
+// 	switch (unit) {
+// 		case "Кб":
+// 		case "кб":
+// 		case "кБ":
+// 		case "КБ":
+// 			return "В " + size + " Кб" + " содержится " + size * 1024 + " байт";
+// 			break;
+// 		case "Мб":
+// 		case "мб":
+// 		case "мБ":
+// 		case "МБ":
+// 			return "В " + size + " Мб" + " содержится " + size * 2 ** 20 + " байт";
+// 			break;
+// 		case "Гб":
+// 		case "гб":
+// 		case "гБ":
+// 		case "ГБ":
+// 			return "В " + size + " Гб" + " содержится " + size * 2 ** 30 + " байт";
+// 			break;
+// 		default: 
+// 			alert ("Корявые данные");
+// 			break;
+// 	}
+// }
+// console.log(howMuchIs(500, "Гб"));
 
 //Создайте функцию, которая ищет наибольший общий делитель двух чисел.
 
-function biggestOne(a, b) {
-	let x = 1;
-	let y = 0;
-	while (x <= a) {
-		if(a % x == 0 && b % x == 0) {
-			y = x;
-		}
-		x++;
-	}
-	console.log(y);
-}
-biggestOne(30, 65);
+// function biggestDivisor(x, y) {
+// 	let a = 1;
+// 	let b = 0;
+// 	while (a <= x) {
+// 		if(x % a == 0 && y % a == 0) {
+// 			b = a;
+// 		}
+// 		a++;
+// 	}
+// 	console.log(b);
+// }
+// biggestDivisor(45, 4);
 
 // Решите предыдущую задачу через рекурсию.
-function biggestOneAgain(a, b) {
-if (a == 0) {
-	return b;
-} else {
-	return biggestOneAgain(b % a, a);
-}
-}
-console.log(biggestOneAgain(30, 65));
+
+// function divisorAgain(a, b) {
+// 	if (a == 0) {
+// 		return b;
+// 	} else {
+// 		return divisorAgain(b % a, a);
+// 	}
+// }
+// console.log(divisorAgain(34, 50));
 
 //Создайте функцию words(),  которая в зависимости от переданного в нее целого числа n, 
 //будет выводить слово «карандаш» в правильной форме («12 карандашей», но «22 карандаша»).
-function words(n) {
-	if (n >= 5 && n <= 20 || n % 10 == 0 || n == 0 || n % 10 > 4 && n % 10 <= 9) {
-	return n + ' карандашей';
-} else if (n > 1 && n < 5 || n % 10 < 5 && n % 10 > 1) {
-	return n + ' карандаша';
-} else if (n == 1 || n % 10 == 1) {
-	return n + ' карандаш';
-}
-}
-console.log(words(100000));
+
+// function words(n) {
+// 	if (n >= 5 && n <= 20 || n % 10 == 0 || n == 0 || n % 10 > 4 && n % 10 <= 9) {
+// 	return n + ' карандашей';
+// } else if (n > 1 && n < 5 || n % 10 < 5 && n % 10 > 1) {
+// 	return n + ' карандаша';
+// } else if (n == 1 || n % 10 == 1) {
+// 	return n + ' карандаш';
+// }
+// }
+// console.log(words(1));
 
 //Создайте функцию, которая проверяет, можно ли представить число в виде суммы квадратов двух целых однозначных чисел.
-function getSquare(z) {
+
+function sumOfSquares(z) {
     for(let x = 1; x < z; x++ ){
         for(let y = z; y >= x; y--){
             if(x * x + y * y == z){
@@ -256,4 +278,4 @@ function getSquare(z) {
     }
     return false;
 }
-console.log(getSquare(8));
+console.log(sumOfSquares(34));
