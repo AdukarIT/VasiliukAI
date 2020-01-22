fd2b5ca568873a39d0e43df9d7a8248375e44dc2
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2020-01-22 14:27:25 +0200

    Create temp.js

---
+++ sppd/temp.js
@@ -0,0 +1,28 @@
+let cardTheme = [
+	Neutral: {
+		Theme image: ,
+		Theme color: 'Gray',
+		Theme decription: ,
+		Cards: [{
+			Card name: ,
+			Card image: ,
+			Attack status: ('Fighter', 'Killer', 'Tank', 'Ranged', 'Spell', 'Totem', 'Trap'),
+			Rarity: ('Common', 'Rare', 'Epic', 'Legendary'),
+			Card description: ,
+		 	Ability: ,
+		 	Energy Cost: ,
+		 	Health: ,
+		 	Attack: ,
+		 	Range: ,
+		 	Radius: ,
+		 	Charge: ,
+		 	Time: , 
+		 	Units: ,
+		}]
+	},
+	Adventure: ,
+	SciFi: ,
+	Mystery: ,
+	Fantasy: ,
+	Superheroes:
+];
